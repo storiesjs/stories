bbccd888c2a48f9fd9547ae1503e81a1cc1857c8
akserg <akserg@gmail.com>
2022-04-07 10:28:17 +0200

    Direct import of StoriesVue

--- demo/demo-vue/src/main.ts
+++ demo/demo-vue/src/main.ts
@@ -1,10 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue'
 import Stories from './Stories.vue'
-import * as StoriesJsVue from '@stories-js/vue';
+import { StoriesVue } from '@stories-js/vue';
 
 if (process.env.VUE_APP_STORIES) {
-  createApp(Stories).use(StoriesJsVue.StoriesVue).mount("#app");
+  createApp(Stories).use(StoriesVue).mount("#app");
 } else {
   createApp(App).mount("#app");
 }
